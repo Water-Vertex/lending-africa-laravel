d242ef3f6ae526981ff10306ff045df527f1a08f
Midha Ahsan <midhaahsan896@gmail.com>
2026-07-04 19:00:53 +0500

    roles and user crud

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,47 @@
 <?php
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\AdminDashboardController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
-Route::post('admin/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+Route::post('/admin/login', [AuthController::class, 'login']);
+
+/*
+|--------------------------------------------------------------------------
+| Protected Routes (Sanctum)
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Auth
+    Route::post('/admin/logout', [AuthController::class, 'logout']);
+    Route::get('/admin/me',      [AuthController::class, 'me']);
+
+    // Dashboard
+    Route::get('/admin/dashboard/stats', [AdminDashboardController::class, 'stats']);
+       // Roles
+    Route::get('/admin/roles',           [RoleController::class, 'index']);
+    Route::post('/admin/roles',          [RoleController::class, 'store']);
+    Route::get('/admin/roles/{id}',      [RoleController::class, 'show']);
+    Route::put('/admin/roles/{id}',      [RoleController::class, 'update']);
+    Route::delete('/admin/roles/{id}',   [RoleController::class, 'destroy']);
+
+
+
+
+    // Users
+    Route::get('/admin/users',           [UserController::class, 'index']);
+    Route::post('/admin/users',          [UserController::class, 'store']);
+    Route::get('/admin/users/{id}',      [UserController::class, 'show']);
+    Route::put('/admin/users/{id}',      [UserController::class, 'update']);
+    Route::delete('/admin/users/{id}',   [UserController::class, 'destroy']);
+
+
+});
