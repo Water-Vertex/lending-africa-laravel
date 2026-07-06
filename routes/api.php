a0bdafc42d39c6d2b30706c0b4d4025f02f1670b
Midha Ahsan <midhaahsan896@gmail.com>
2026-07-06 19:35:40 +0500

    collaterals work

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AdminDashboardController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CollateralController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -42,6 +43,19 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/admin/users/{id}',      [UserController::class, 'show']);
     Route::put('/admin/users/{id}',      [UserController::class, 'update']);
     Route::delete('/admin/users/{id}',   [UserController::class, 'destroy']);
+ // Collaterals (collaterals table) 
+    Route::get('/admin/collaterals',           [CollateralController::class, 'index']);
+    Route::post('/admin/collaterals',          [CollateralController::class, 'store']);
+    Route::get('/admin/collaterals/{id}',      [CollateralController::class, 'show']);
+    Route::put('/admin/collaterals/{id}',      [CollateralController::class, 'update']);
+    Route::delete('/admin/collaterals/{id}',   [CollateralController::class, 'destroy']);
+
+    // Collateral Types (collateral_types table) 
+    Route::get('/admin/collateral-types',           [CollateralController::class, 'typesIndex']);
+    Route::post('/admin/collateral-types',          [CollateralController::class, 'typesStore']);
+    Route::get('/admin/collateral-types/{id}',      [CollateralController::class, 'typesShow']);
+    Route::put('/admin/collateral-types/{id}',      [CollateralController::class, 'typesUpdate']);
+    Route::delete('/admin/collateral-types/{id}',   [CollateralController::class, 'typesDestroy']);
 
 
 });
